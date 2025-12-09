fa4986e6468e6f4b97807171374f629f65a0eab7
Ingmar Splitt <orgua@users.noreply.github.com>
2025-12-09 15:31:11 +0100

    update ruff-rules

--- examples/monitor.py
+++ examples/monitor.py
@@ -1,6 +1,5 @@
 from bistmon import concurrent_monitor
-from bistmon import log
 
-log.info("Starting Serial Monitor")
+print("Starting Serial Monitor")
 
 concurrent_monitor.monitor_serial("/dev/tty.usbmodem11102")
